1a1612be124ab608129df5be73123a7326649ed8
MarlonSelvi <marlonsb0502@gmail.com>
2025-10-06 18:56:55 -0500

    feat: Magenta & Blue books data extraction

---
+++ tuutrag/get_pdf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Downloads PDFs, storing them in PDF folder
+for i in $(seq 1 40);
+do
+        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' Magenta_metadata.json)
+        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' Magenta_metadata.json)
+        name="${name//[. ]/_}"
+        name="${name//_pdf/}"
+        curl "$link" -o "../data/ccsds/blue/${name}.pdf"
+done
+
+for i in $(seq 1 99);
+do
+        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' Blue_metadata.json)
+        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' Blue_metadata.json)
+        name="${name//[. ]/_}"
+        name="${name//_pdf/}"
+        curl "$link" -o "../data/ccsds/magenta/${name}.pdf"
+done
+
