4f994efab95c7acfdad711cebfdcb5f89842e835
MarlonSelvi <marlonsb0502@gmail.com>
2025-10-06 19:00:42 -0500

    feat: Magenta & Blue books data extraction

--- tuutrag/get_pdf.sh
+++ tuutrag/get_pdf.sh
@@ -2,19 +2,19 @@
 # Downloads PDFs, storing them in PDF folder
 for i in $(seq 1 40);
 do
-        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' Magenta_metadata.json)
-        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' Magenta_metadata.json)
+        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' ../data/meta/magentabook_meta.json)
+        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' ../data/meta/magentabook_meta.json)
         name="${name//[. ]/_}"
         name="${name//_pdf/}"
-        curl "$link" -o "../data/ccsds/blue/${name}.pdf"
+        curl "$link" -o "../data/ccsds/magenta/${name}.pdf"
 done
 
 for i in $(seq 1 99);
 do
-        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' Blue_metadata.json)
-        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' Blue_metadata.json)
+        link=$(jq -r --arg key "$i" '.[$key]["Link:"]' ../data/meta/bluebook_meta.json)
+        name=$(jq -r --arg key "$i" '.[$key]["PDF Name:"]' ../data/meta/bluebook_meta.json)
         name="${name//[. ]/_}"
         name="${name//_pdf/}"
-        curl "$link" -o "../data/ccsds/magenta/${name}.pdf"
+        curl "$link" -o "../data/ccsds/blue/${name}.pdf"
 done
 
