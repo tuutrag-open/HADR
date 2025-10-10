de848016911e41f509060513b21f94e5d8fc931e
Marlon Selvi <147783303+MarlonSelvi@users.noreply.github.com>
2025-10-10 15:06:46 -0500

    feat: Magenta & Blue books data exctractor (#12)
    * feat: Magenta & Blue books data extractor
    
    * feat: Magenta & Blue books data extractor
    
    * feat: Magenta & Blue books data extractor
    
    * feat: Magenta & Blue books data extractor
    
    * Rename get_pdf.sh to scraper.sh
    
    * Rename scrapers.py to scraper.py
    
    * Update scraper.sh
    
    * Update scraper.py
    
    ---------
    
    Co-authored-by: pablo <70508631+bedolpab@users.noreply.github.com>

---
+++ scripts/scraper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ================================================================
+# path: scripts/scraper.sh
+# brief: Downloads PDFs, storing them in PDF folder
+# ================================================================
+for i in $(seq 1 40);
+do
+       link=$(jq -r ".[$i - 1][\"Link:\"]" ../data/meta/magentabook_meta.json)
+       name=$(jq -r ".[$i - 1][\"PDF Name:\"]" ../data/meta/magentabook_meta.json)
+       name="${name//[. ]/_}"
+       name="${name//_pdf/}"
+       curl "$link" -o "../data/ccsds/magenta/${name}.pdf"
+
+
+done
+
+
+for i in $(seq 1 99);
+do
+       link=$(jq -r ".[$i - 1][\"Link:\"]" ../data/meta/bluebook_meta.json)
+       name=$(jq -r ".[$i - 1][\"PDF Name:\"]" ../data/meta/bluebook_meta.json)
+       name="${name//[. ]/_}"
+       name="${name//_pdf/}"
+       curl "$link" -o "../data/ccsds/blue/${name}.pdf"
+done
+
+
