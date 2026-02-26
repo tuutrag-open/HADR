fd6d6c78014e614c4e9cca7bce5778c07daec90c
pablo <70508631+bedolpab@users.noreply.github.com>
2026-02-26 14:38:08 -0600

    Research embedding work (#41)

--- tuutrag/utils.py
+++ tuutrag/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from configparser import ConfigParser
 from datetime import datetime
+import json
 
 
 def log_timestamp(message: str) -> None:
@@ -19,3 +20,20 @@ def load_config(config_path: str) -> ConfigParser:
 
     config.read(config_file)
     return config
+
+def get_type(uuid_prefix: str) -> str:
+    def load_json(path):
+        with open(Path(path)) as f:
+            return json.load(f)
+
+    REQUIREMENT_UUIDS = {obj["uuid"] for obj in load_json("../data/requirement_uuid_mapping.json")}
+    SUPPLEMENTAL_UUIDS = {obj["uuid"] for obj in load_json("../data/supplemental_uuid_mapping.json")}
+    TEST_UUIDS = {obj["uuid"] for obj in load_json("../data/test_uuid_mapping.json")}
+
+    if uuid_prefix in REQUIREMENT_UUIDS:
+        return "requirement"
+    if uuid_prefix in SUPPLEMENTAL_UUIDS:
+        return "supplemental"
+    if uuid_prefix in TEST_UUIDS:
+        return "test"
+    return "unknown"
